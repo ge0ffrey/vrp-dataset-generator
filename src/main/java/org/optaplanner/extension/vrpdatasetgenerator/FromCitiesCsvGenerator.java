8ee1cd6e1340345248af8c148d13d94865d058d5
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2014-09-02 11:24:13 +0200

    make road and non road versions + clean up belgian cities

--- src/main/java/org/optaplanner/extension/vrpdatasetgenerator/FromCitiesCsvGenerator.java
+++ src/main/java/org/optaplanner/extension/vrpdatasetgenerator/FromCitiesCsvGenerator.java
@@ -65,21 +65,30 @@ public class FromCitiesCsvGenerator extends LoggingMain {
     }
 
     public void generate() {
-        generateVrp(new File("data/raw/belgium-cities.csv"), 100, 10, 250);
-//        generateVrp(new File("data/raw/belgium-cities.csv"), 500, 20, 250);
-//        generateVrp(new File("data/raw/belgium-cities.csv"), 1000, 20, 500);
-//        generateVrp(new File("data/raw/belgium-cities.csv"), 2871, 60, 500);
+        // Air
+        generateVrp(new File("data/raw/belgium-cities.csv"), 100, 10, 250, false, false);
+        generateVrp(new File("data/raw/belgium-cities.csv"), 500, 20, 250, false, false);
+        generateVrp(new File("data/raw/belgium-cities.csv"), 1000, 20, 500, false, false);
+        generateVrp(new File("data/raw/belgium-cities.csv"), 2750, 55, 500, false, false);
+        // Road
+        generateVrp(new File("data/raw/belgium-cities.csv"), 100, 10, 250, true, false);
+//        generateVrp(new File("data/raw/belgium-cities.csv"), 500, 20, 250, true, false);
+//        generateVrp(new File("data/raw/belgium-cities.csv"), 1000, 20, 500, true, false);
+//        generateVrp(new File("data/raw/belgium-cities.csv"), 2750, 55, 500, true, false);
     }
 
-    public void generateVrp(File cityFile, int locationListSize, int vehicleListSize, int capacity) {
+    public void generateVrp(File cityFile, int locationListSize, int vehicleListSize, int capacity, boolean road, boolean scalable) {
         List<City> cityList = readCityFile(cityFile);
         if (locationListSize > cityList.size()) {
             throw new IllegalArgumentException("The locationListSize (" + locationListSize
                     + ") is larger than the cityList size (" + cityList.size() + ").");
         }
+        String suffix = road ? (scalable ? "-roadhub" : "-road") : "";
         String name = cityFile.getName().replaceAll("\\-cities.csv", "")
-                + "-road-n" + locationListSize + "-k" + vehicleListSize;
-        File vrpOutputFile = new File(vehicleRoutingDao.getDataDir(), "import/roaddistance/capacitated/" + name + ".vrp");
+                + suffix + "-n" + locationListSize + "-k" + vehicleListSize;
+        File vrpOutputFile = new File(vehicleRoutingDao.getDataDir(), "import"
+                + (road ? "/roaddistance" : "")
+                + "/capacitated/" + name + ".vrp");
         if (!vrpOutputFile.getParentFile().exists()) {
             throw new IllegalArgumentException("The vrpOutputFile parent directory (" + vrpOutputFile.getParentFile()
                     + ") does not exist.");
@@ -91,8 +100,12 @@ public class FromCitiesCsvGenerator extends LoggingMain {
             vrpWriter.write("COMMENT: Generated for OptaPlanner. Road distance calculated with GraphHopper.\n");
             vrpWriter.write("TYPE: CVRP\n");
             vrpWriter.write("DIMENSION: " + locationListSize + "\n");
-            vrpWriter.write("EDGE_WEIGHT_TYPE: EXPLICIT\n");
-            vrpWriter.write("EDGE_WEIGHT_FORMAT: FULL_MATRIX\n");
+            if (road) {
+                vrpWriter.write("EDGE_WEIGHT_TYPE: EXPLICIT\n");
+                vrpWriter.write("EDGE_WEIGHT_FORMAT: FULL_MATRIX\n");
+            } else {
+                vrpWriter.write("EDGE_WEIGHT_TYPE: EUC_2D\n");
+            }
             vrpWriter.write("CAPACITY: " + capacity + "\n");
             vrpWriter.write("NODE_COORD_SECTION\n");
             double selectionDecrement = (double) locationListSize / (double) cityList.size();
@@ -110,32 +123,48 @@ public class FromCitiesCsvGenerator extends LoggingMain {
                 selection = newSelection;
             }
             cityList = newCityList;
-            vrpWriter.write("EDGE_WEIGHT_SECTION\n");
-            DecimalFormat distanceFormat = new DecimalFormat("0.000");
-            for (City fromCity : cityList) {
-                for (City toCity : cityList) {
-                    double distance;
-                    if (fromCity == toCity) {
-                        distance = 0.0;
-                    } else {
-                        GHRequest request = new GHRequest(fromCity.getLatitude(), fromCity.getLongitude(),
-                                toCity.getLatitude(), toCity.getLongitude())
-                                .setVehicle("car");
-                        GHResponse response = graphHopper.route(request);
-                        if (response.hasErrors()) {
-                            throw new IllegalStateException("GraphHopper gave " + response.getErrors().size()
-                                    + " errors. First error chained.",
-                                    response.getErrors().get(0));
+            if (road) {
+                vrpWriter.write("EDGE_WEIGHT_SECTION\n");
+                DecimalFormat distanceFormat = new DecimalFormat("0.000");
+                for (City fromCity : cityList) {
+                    for (City toCity : cityList) {
+                        double distance;
+                        if (fromCity == toCity) {
+                            distance = 0.0;
+                        } else {
+                            GHRequest request = new GHRequest(fromCity.getLatitude(), fromCity.getLongitude(),
+                                    toCity.getLatitude(), toCity.getLongitude())
+                                    .setVehicle("car");
+                            GHResponse response = graphHopper.route(request);
+                            if (response.hasErrors()) {
+                                throw new IllegalStateException("GraphHopper gave " + response.getErrors().size()
+                                        + " errors. First error chained.",
+                                        response.getErrors().get(0)
+                                );
+                            }
+                            // Distance should be in km, not meter
+                            distance = response.getDistance() / 1000.0;
+                            if (distance == 0.0) {
+                                throw new IllegalArgumentException("The fromCity (" + fromCity
+                                        + ") and toCity (" + toCity + ") are the same.");
+                            }
                         }
-                        // Distance should be in km, not meter
-                        distance = response.getDistance() / 1000.0;
+                        vrpWriter.write(distanceFormat.format(distance) + " ");
                     }
-                    vrpWriter.write(distanceFormat.format(distance) + " ");
+                    vrpWriter.write("\n");
+                    logger.info("All distances calculated for city ({}).", fromCity);
                 }
-                vrpWriter.write("\n");
-                logger.info("All distances calculated for city ({}).", fromCity);
-            }
+            } else {
+                for (City fromCity : cityList) {
+                    for (City toCity : cityList) {
+                        if (fromCity != toCity && fromCity.getDistance(toCity) == 0) {
+                            throw new IllegalArgumentException("The fromCity (" + fromCity
+                                    + ") and toCity (" + toCity + ") are the same.");
+                        }
+                    }
+                }
 
+            }
             vrpWriter.write("DEMAND_SECTION\n");
             // maximumDemand is 2 times the averageDemand. And the averageDemand is 2/3th of available capacity
             int maximumDemand = (4 * vehicleListSize * capacity) / (locationListSize * 3);
