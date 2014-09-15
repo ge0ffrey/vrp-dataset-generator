fb4671be7ab9126a9ed1bb52663da6bcba3d941f
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2014-09-15 15:33:06 +0200

    generate segmented km too

--- src/main/java/org/optaplanner/extension/vrpdatasetgenerator/FromBelgiumCitiesCsvGenerator.java
+++ src/main/java/org/optaplanner/extension/vrpdatasetgenerator/FromBelgiumCitiesCsvGenerator.java
@@ -74,107 +74,23 @@ public class FromBelgiumCitiesCsvGenerator extends LoggingMain {
     }
 
     public void generate() {
-        generateVrp(new File("data/raw/belgium-cities.csv"), null, 50, 10, 125, GenerationDistanceType.AIR_DISTANCE);
-        generateVrp(new File("data/raw/belgium-cities.csv"), null, 100, 10, 250, GenerationDistanceType.AIR_DISTANCE);
-        generateVrp(new File("data/raw/belgium-cities.csv"), null, 500, 20, 250, GenerationDistanceType.AIR_DISTANCE);
-        generateVrp(new File("data/raw/belgium-cities.csv"), null, 1000, 20, 500, GenerationDistanceType.AIR_DISTANCE);
-        generateVrp(new File("data/raw/belgium-cities.csv"), null, 2750, 55, 500, GenerationDistanceType.AIR_DISTANCE);
+        File locationFile = new File("data/raw/belgium-cities.csv");
+        File hubFile = new File("data/raw/belgium-hubs.txt");
 
-        generateVrp(new File("data/raw/belgium-cities.csv"), null, 50, 10, 125, GenerationDistanceType.ROAD_DISTANCE_KM);
-        generateVrp(new File("data/raw/belgium-cities.csv"), null, 100, 10, 250, GenerationDistanceType.ROAD_DISTANCE_KM);
-        generateVrp(new File("data/raw/belgium-cities.csv"), null, 500, 20, 250, GenerationDistanceType.ROAD_DISTANCE_KM);
-        generateVrp(new File("data/raw/belgium-cities.csv"), null, 1000, 20, 500, GenerationDistanceType.ROAD_DISTANCE_KM);
-        generateVrp(new File("data/raw/belgium-cities.csv"), null, 2750, 55, 500, GenerationDistanceType.ROAD_DISTANCE_KM);
-
-        generateVrp(new File("data/raw/belgium-cities.csv"), new File("data/raw/belgium-hubs.txt"), 50, 10, 125, GenerationDistanceType.SEGMENTED_ROAD_DISTANCE_TIME);
-        generateVrp(new File("data/raw/belgium-cities.csv"), new File("data/raw/belgium-hubs.txt"), 100, 10, 250, GenerationDistanceType.SEGMENTED_ROAD_DISTANCE_TIME);
-        generateVrp(new File("data/raw/belgium-cities.csv"), new File("data/raw/belgium-hubs.txt"), 500, 20, 250, GenerationDistanceType.SEGMENTED_ROAD_DISTANCE_TIME);
-        generateVrp(new File("data/raw/belgium-cities.csv"), new File("data/raw/belgium-hubs.txt"), 1000, 20, 500, GenerationDistanceType.SEGMENTED_ROAD_DISTANCE_TIME);
-        generateVrp(new File("data/raw/belgium-cities.csv"), new File("data/raw/belgium-hubs.txt"), 2750, 55, 500, GenerationDistanceType.SEGMENTED_ROAD_DISTANCE_TIME);
-
-        generateVrp(new File("data/raw/belgium-cities.csv"), null, 50, 10, 125, GenerationDistanceType.ROAD_DISTANCE_TIME);
-        generateVrp(new File("data/raw/belgium-cities.csv"), null, 100, 10, 250, GenerationDistanceType.ROAD_DISTANCE_TIME);
-        generateVrp(new File("data/raw/belgium-cities.csv"), null, 500, 20, 250, GenerationDistanceType.ROAD_DISTANCE_TIME);
-        generateVrp(new File("data/raw/belgium-cities.csv"), null, 1000, 20, 500, GenerationDistanceType.ROAD_DISTANCE_TIME);
-        generateVrp(new File("data/raw/belgium-cities.csv"), null, 2750, 55, 500, GenerationDistanceType.ROAD_DISTANCE_TIME);
-
-        generateVrp(new File("data/raw/belgium-cities.csv"), new File("data/raw/belgium-hubs.txt"), 50, 10, 125, GenerationDistanceType.SEGMENTED_ROAD_DISTANCE_TIME);
-        generateVrp(new File("data/raw/belgium-cities.csv"), new File("data/raw/belgium-hubs.txt"), 100, 10, 250, GenerationDistanceType.SEGMENTED_ROAD_DISTANCE_TIME);
-        generateVrp(new File("data/raw/belgium-cities.csv"), new File("data/raw/belgium-hubs.txt"), 500, 20, 250, GenerationDistanceType.SEGMENTED_ROAD_DISTANCE_TIME);
-        generateVrp(new File("data/raw/belgium-cities.csv"), new File("data/raw/belgium-hubs.txt"), 1000, 20, 500, GenerationDistanceType.SEGMENTED_ROAD_DISTANCE_TIME);
-        generateVrp(new File("data/raw/belgium-cities.csv"), new File("data/raw/belgium-hubs.txt"), 2750, 55, 500, GenerationDistanceType.SEGMENTED_ROAD_DISTANCE_TIME);
+        generateVrp(locationFile, hubFile, 50, 10, 125);
+        generateVrp(locationFile, hubFile, 100, 10, 250);
+        generateVrp(locationFile, hubFile, 500, 20, 250);
+        generateVrp(locationFile, hubFile, 1000, 20, 500);
+        generateVrp(locationFile, hubFile, 2750, 55, 500);
     }
 
-    private static enum GenerationDistanceType {
-        AIR_DISTANCE,
-        ROAD_DISTANCE_KM,
-        ROAD_DISTANCE_TIME,
-        SEGMENTED_ROAD_DISTANCE_KM,
-        SEGMENTED_ROAD_DISTANCE_TIME;
+    public void generateVrp(File locationFile, File hubFile, int locationListSize, int vehicleListSize, int capacity) {
 
-        public String getFileSuffix() {
-            switch (this) {
-                case AIR_DISTANCE:
-                    return "";
-                case ROAD_DISTANCE_KM:
-                    return "-road-km";
-                case ROAD_DISTANCE_TIME:
-                    return "-road-time";
-                case SEGMENTED_ROAD_DISTANCE_KM:
-                    return "-segmentedRoad-km";
-                case SEGMENTED_ROAD_DISTANCE_TIME:
-                    return "-segmentedRoad-time";
-                default:
-                    throw new IllegalStateException("The generationDistanceType (" + this
-                            + ") is not implemented.");
-
-            }
-        }
-
-        public boolean isRoad() {
-            return this != AIR_DISTANCE;
-        }
-
-        public boolean isSegmented() {
-            return this == SEGMENTED_ROAD_DISTANCE_KM || this == SEGMENTED_ROAD_DISTANCE_TIME;
-        }
-
-        public double extractDistance(GHResponse response) {
-            switch (this) {
-                case AIR_DISTANCE:
-                    throw new IllegalStateException("The generationDistanceType (" + this
-                            + ") does not support GHResponse.");
-                case ROAD_DISTANCE_KM:
-                case SEGMENTED_ROAD_DISTANCE_KM:
-                    // Distance should be in km, not meter
-                    return response.getDistance() / 1000.0;
-                case ROAD_DISTANCE_TIME:
-                case SEGMENTED_ROAD_DISTANCE_TIME:
-                    return response.getMillis() / 1000.0;
-                default:
-                    throw new IllegalStateException("The generationDistanceType (" + this
-                            + ") is not implemented.");
-
-            }
-        }
-
-        public String getUnitOfMeasurement() {
-            switch (this) {
-                case AIR_DISTANCE:
-                    throw new IllegalStateException("The generationDistanceType (" + this
-                            + ") does unit of measurement.");
-                case ROAD_DISTANCE_KM:
-                case SEGMENTED_ROAD_DISTANCE_KM:
-                    return "km";
-                case ROAD_DISTANCE_TIME:
-                case SEGMENTED_ROAD_DISTANCE_TIME:
-                    return "sec";
-                default:
-                    throw new IllegalStateException("The generationDistanceType (" + this
-                            + ") is not implemented.");
-
-            }
-        }
+        generateVrp(locationFile, null, locationListSize, vehicleListSize, capacity, GenerationDistanceType.AIR_DISTANCE);
+        generateVrp(locationFile, null, locationListSize, vehicleListSize, capacity, GenerationDistanceType.ROAD_DISTANCE_KM);
+        generateVrp(locationFile, hubFile, locationListSize, vehicleListSize, capacity, GenerationDistanceType.SEGMENTED_ROAD_DISTANCE_KM);
+        generateVrp(locationFile, null, locationListSize, vehicleListSize, capacity, GenerationDistanceType.ROAD_DISTANCE_TIME);
+        generateVrp(locationFile, hubFile, locationListSize, vehicleListSize, capacity, GenerationDistanceType.SEGMENTED_ROAD_DISTANCE_TIME);
     }
 
     public void generateVrp(File locationFile, File hubFile, int locationListSize, int vehicleListSize, int capacity, GenerationDistanceType distanceType) {
@@ -564,6 +480,78 @@ public class FromBelgiumCitiesCsvGenerator extends LoggingMain {
             return "" + latitude + "," + longitude;
         }
 
+    }
+
+    private static enum GenerationDistanceType {
+        AIR_DISTANCE,
+        ROAD_DISTANCE_KM,
+        ROAD_DISTANCE_TIME,
+        SEGMENTED_ROAD_DISTANCE_KM,
+        SEGMENTED_ROAD_DISTANCE_TIME;
+
+        public String getFileSuffix() {
+            switch (this) {
+                case AIR_DISTANCE:
+                    return "";
+                case ROAD_DISTANCE_KM:
+                    return "-road-km";
+                case ROAD_DISTANCE_TIME:
+                    return "-road-time";
+                case SEGMENTED_ROAD_DISTANCE_KM:
+                    return "-segmentedRoad-km";
+                case SEGMENTED_ROAD_DISTANCE_TIME:
+                    return "-segmentedRoad-time";
+                default:
+                    throw new IllegalStateException("The generationDistanceType (" + this
+                            + ") is not implemented.");
+
+            }
+        }
+
+        public boolean isRoad() {
+            return this != AIR_DISTANCE;
+        }
+
+        public boolean isSegmented() {
+            return this == SEGMENTED_ROAD_DISTANCE_KM || this == SEGMENTED_ROAD_DISTANCE_TIME;
+        }
+
+        public double extractDistance(GHResponse response) {
+            switch (this) {
+                case AIR_DISTANCE:
+                    throw new IllegalStateException("The generationDistanceType (" + this
+                            + ") does not support GHResponse.");
+                case ROAD_DISTANCE_KM:
+                case SEGMENTED_ROAD_DISTANCE_KM:
+                    // Distance should be in km, not meter
+                    return response.getDistance() / 1000.0;
+                case ROAD_DISTANCE_TIME:
+                case SEGMENTED_ROAD_DISTANCE_TIME:
+                    return response.getMillis() / 1000.0;
+                default:
+                    throw new IllegalStateException("The generationDistanceType (" + this
+                            + ") is not implemented.");
+
+            }
+        }
+
+        public String getUnitOfMeasurement() {
+            switch (this) {
+                case AIR_DISTANCE:
+                    throw new IllegalStateException("The generationDistanceType (" + this
+                            + ") does unit of measurement.");
+                case ROAD_DISTANCE_KM:
+                case SEGMENTED_ROAD_DISTANCE_KM:
+                    return "km";
+                case ROAD_DISTANCE_TIME:
+                case SEGMENTED_ROAD_DISTANCE_TIME:
+                    return "sec";
+                default:
+                    throw new IllegalStateException("The generationDistanceType (" + this
+                            + ") is not implemented.");
+
+            }
+        }
     }
 
 }
